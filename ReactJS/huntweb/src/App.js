8a887083f8d6181ee29701d601cf398b4fb77779
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2020-01-15 15:35:02 -0300

    [React] Creating a simple application

--- ReactJS/huntweb/src/App.js
+++ ReactJS/huntweb/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Header from './components/Header';
+import Routes from './routes';
+
 import './styles.css';
+
+import Header from './components/Header';
+import Main from './pages/main';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes />
     </div>
   );
 }
